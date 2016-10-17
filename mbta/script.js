a91ee29f6bd8d67aa8c950c67ccd93ec3d0a6c08
Jeremy Su <jeremycksu@yahoo.com>
2016-10-17 19:04:28 -0400

    Have map, navigator, and simple marker on self

--- mbta/script.js
+++ mbta/script.js
@@ -16,37 +16,44 @@ document.write(" ",month,"/",day,"/",year," ");
 var map;
 var my_lat= -999999;
 var my_long = -999999;
-//var loc_lat = loc.coords.latitude;
-//var loc_long = loc.coords.latitude;
-//console.log(myLocation);
+var infowindow = new google.maps.InfoWindow();
 
 var myOptions = {
 						zoom: 13, 
-						center: {lat: -34.397, lng: 150.644},
 						mapTypeId: google.maps.MapTypeId.ROADMAP
 					};
 function initMap() {
-        	//map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
         	getLocation();
         }
 
 function getLocation(){
-	
-	console.log("Hit me 1");
 	if(navigator.geolocation){
-			console.log("Navigator is compatible with this browser");
 			navigator.geolocation.getCurrentPosition(function(pos) {
-			console.log("Hit me 2");
 			my_lat = pos.coords.latitude;
 			my_long = pos.coords.longitude;
-
+			renderMap(my_lat,my_long);
 		})
-			console.log("hit me 3");
 	}else{
 		console.log("Geolocation not supported on this browser.");
 	}
-	
 }
+
+function renderMap(lat,long){
+	myLoc = new google.maps.LatLng(lat,long);
+	myOptions["center"] = myLoc;
+	map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+	marker = new google.maps.Marker({
+		position: myLoc,
+		title:"Your location is " + lat + ", " + long
+	})
+	marker.setMap(map);
+
+	google.maps.event.addListener(marker, "click", function(){
+		infowindow.setContent(marker.title);
+		infowindow.open(map,marker);
+	})
+}
+
 
 
 
