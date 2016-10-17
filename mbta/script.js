fcce6ee22fdd3234d80169264f746665bfa6f699
Jeremy Su <jeremycksu@yahoo.com>
2016-10-16 20:14:21 -0400

    trying to get git to work

---
+++ mbta/script.js
@@ -0,0 +1,24 @@
+
+test = new Date();
+month = test.getMonth();
+month = (month * 1) + 1;
+day = test.getDate();
+year = test.getFullYear();
+document.write(" ",month,"/",day,"/",year," ");
+
+
+
+
+
+
+/*
+var map;
+      	function initMap() {
+        	map = new google.maps.Map(document.getElementById('map'), {
+          	center: {lat: -34.397, lng: 150.644},
+          	zoom: 8
+        	});
+      }
+*/
+
+
