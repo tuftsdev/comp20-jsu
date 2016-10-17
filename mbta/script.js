deecd9365a005b6ece3a57a302f29df41d06ac5b
Jeremy Su <jeremycksu@yahoo.com>
2016-10-16 21:33:14 -0400

    navigator does not function

--- mbta/script.js
+++ mbta/script.js
@@ -14,8 +14,8 @@ document.write(" ",month,"/",day,"/",year," ");
 
 
 var map;
-var my_lat= 0;
-var my_long = 0;
+var my_lat= -999999;
+var my_long = -999999;
 //var loc_lat = loc.coords.latitude;
 //var loc_long = loc.coords.latitude;
 //console.log(myLocation);
@@ -26,21 +26,31 @@ var myOptions = {
 						mapTypeId: google.maps.MapTypeId.ROADMAP
 					};
 function initMap() {
-        	map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
+        	//map = new google.maps.Map(document.getElementById('map_canvas'), myOptions);
         	getLocation();
         }
 
 function getLocation(){
+	
+	console.log("Hit me 1");
 	if(navigator.geolocation){
+			console.log("Navigator is compatible with this browser");
 			navigator.geolocation.getCurrentPosition(function(pos) {
+			console.log("Hit me 2");
 			my_lat = pos.coords.latitude;
 			my_long = pos.coords.longitude;
 
 		})
-		document.write(my_lat,my_long);
+			console.log("hit me 3");
 	}else{
 		console.log("Geolocation not supported on this browser.");
 	}
+	
 }
+
+
+
+
+
 
 
